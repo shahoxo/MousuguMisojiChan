75534b70f2b16bd7959f65355a446ff91e56fdfc
Jun Sitow <jshitou@aiming-inc.com>
2017-01-24 19:50:29 +0900

    wip

---
+++ Assets/AgeIncrementor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void AgeHandler(int year);
+public class AgeIncrementor : MonoBehaviour {
+	
+	public event AgeHandler OnIncrementAge;
+
+	void Start() {
+		//OnIncrementAge += GetComponent<FightController>().IncrementAge;
+	}
+	public void IncrementAge(int year) {
+		// fire incrementing
+		Debug.Log(year);
+		//OnIncrementAge(year);
+	}
+
+	
+}
---
+++ Assets/FightController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FightController : MonoBehaviour {
+	public int age = 0;
+
+	void Awake() {
+		age = 0;
+	}
+
+	void Update() {
+		
+	}
+
+	public void IncrementAge(int year) {
+		age += year;
+		Debug.Log(age);
+	}
+}
