309fb4545a15d82b2f7be6e0162a55e32ea5db80
Jun Sitow <jshitou@aiming-inc.com>
2017-01-26 14:46:01 +0900

    wip

---
+++ Assets/MisojiLobbyHook.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class MisojiLobbyHook : Prototype.NetworkLobby.LobbyHook {
+
+	public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer) {
+		if(lobbyPlayer == null)	
+			return;
+		Prototype.NetworkLobby.LobbyPlayer _lobbyPlayer = lobbyPlayer.GetComponent<Prototype.NetworkLobby.LobbyPlayer>();
+		if(_lobbyPlayer != null)
+			GameManager.AddPlayer(gamePlayer, _lobbyPlayer.nameInput.text, _lobbyPlayer.playerControllerId);
+	}
+}
